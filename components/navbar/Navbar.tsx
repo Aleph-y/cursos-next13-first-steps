f6ea66e739a2582153adf2c9fa9cd1b1d8b6137d
Aleph Mora <alejandroo.dev@gmail.com>
2023-09-09 15:36:45 -0600

    feat: use next links in general navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,21 +1,32 @@
 import type { FC, ReactNode } from 'react'
+import Link from 'next/link'
+import { HomeIcon } from '@primer/octicons-react'
+
+interface NavItemsType {
+  path: string
+  display: string
+}
+
+const navItems: NavItemsType[] = [
+  { path: '/about', display: 'About' },
+  { path: '/contact', display: 'Contact' },
+  { path: '/pricing', display: 'Pricing' },
+]
 
 export const Navbar: FC = (): ReactNode => {
   return (
     <nav className="flex bg-blue-600 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href="/" className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1" />
-
-      <a className="mr-2" href="/about">
-        About
-      </a>
-      <a className="mr-2" href="/contact">
-        Contact
-      </a>
-      <a className="mr-2" href="/pricing">
-        Pricing
-      </a>
+      {navItems.map(({ path, display }: NavItemsType) => (
+        <Link key={path} className="mr-2" href={path}>
+          {display}
+        </Link>
+      ))}
     </nav>
   )
 }
