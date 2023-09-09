815d0d8f50d006b5835b17d274de0ad56e82ee66
Aleph Mora <alejandroo.dev@gmail.com>
2023-09-09 14:58:22 -0600

    feat: add navbar for general layout

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from '@/components'
 import type { FC, ReactNode } from 'react'
 
 export interface GeneralLayoutProps {
@@ -6,9 +7,13 @@ export interface GeneralLayoutProps {
 
 export const GeneralLayout: FC<GeneralLayoutProps> = ({ children }): ReactNode => {
   return (
-    <html lang="es">
-      <body>{children}</body>
-    </html>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span>Fola mundo</span>
+        {children}
+      </main>
+    </>
   )
 }
 
